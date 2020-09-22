6556364d24d44b8941bb09abd3e63bc9bedc3c21
IMZCC <i@imzcc.com>
2020-09-22 15:03:51 +0800

    两数之和

---
+++ LeetCode/0001_两数之和/Solution.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+}
